02877522894bc6e6b869fb0d76707018d6c59717
Michał Kopeć <michal@nozomi.space>
2021-06-07 14:49:12 +0200

    Revert "rts54hub: Allow updating the RTD21xx devices in background mode"
    This reverts commit 7a8c61a5c3ab26e4dcc19fb2509a4bce663f3135.

--- plugins/rts54hub/fu-plugin-rts54hub.c
+++ plugins/rts54hub/fu-plugin-rts54hub.c
@@ -9,8 +9,7 @@
 #include "fu-plugin-vfuncs.h"
 
 #include "fu-rts54hub-device.h"
-#include "fu-rts54hub-rtd21xx-background.h"
-#include "fu-rts54hub-rtd21xx-foreground.h"
+#include "fu-rts54hub-rtd21xx-device.h"
 
 void
 fu_plugin_init (FuPlugin *plugin)
@@ -18,8 +17,7 @@ fu_plugin_init (FuPlugin *plugin)
 	FuContext *ctx = fu_plugin_get_context (plugin);
 	fu_plugin_set_build_hash (plugin, FU_BUILD_HASH);
 	fu_plugin_add_device_gtype (plugin, FU_TYPE_RTS54HUB_DEVICE);
-	fu_plugin_add_device_gtype (plugin, FU_TYPE_RTS54HUB_RTD21XX_BACKGROUND);
-	fu_plugin_add_device_gtype (plugin, FU_TYPE_RTS54HUB_RTD21XX_FOREGROUND);
+	fu_plugin_add_device_gtype (plugin, FU_TYPE_RTS54HUB_RTD21XX_DEVICE);
 	fu_context_add_quirk_key (ctx, "Rts54TargetAddr");
 	fu_context_add_quirk_key (ctx, "Rts54I2cSpeed");
 	fu_context_add_quirk_key (ctx, "Rts54RegisterAddrLen");
